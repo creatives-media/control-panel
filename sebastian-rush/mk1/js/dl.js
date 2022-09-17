3975a759a262c1f026723bba26708f9aba4e000a
sebaptcd <dev@sebastian.ghiveci.com>
2022-09-17 19:09:45 +0200

    Release 〢 1.0.1

--- sebastian-rush/mk1/js/dl.js
+++
@@ -1,7 +0,0 @@
-function dlManager() {
-    let protocol = 'https://';
-    let host = 'p.ghiveci.com/';
-    let dlLocation = protocol+host+plan+account+action+dlID;
-
-    window.open(dlLocation, "_blank");
-}
